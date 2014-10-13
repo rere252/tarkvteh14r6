d195b42910ccd9df6a239a88f4e7945683d18569
airis94 <airis.kruus@gmail.com>
2014-10-14 00:03:05 +0300

    Update IntroUI.java

--- POS/src/ee/ut/math/tvt/beeroverflow/IntroUI.java
+++ POS/src/ee/ut/math/tvt/beeroverflow/IntroUI.java
@@ -2,31 +2,40 @@ package ee.ut.math.tvt.beeroverflow;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 public class IntroUI extends JPanel{
 	
-	public static void createWindow(){
+	public static void createWindow() throws IOException{
+		ImageIcon img = new ImageIcon("img//beer.jpg");
+		JLabel pilt = new JLabel(img);
+		pilt.setIcon(img);
+		File file = new File("application.properties");
+		FileInputStream fileInput = new FileInputStream(file);
+		Properties properties = new Properties();
+		properties.load(fileInput);
+		fileInput.close();
+		Enumeration<Object> enuKeys = properties.keys();
+		JLabel textLabel = new JLabel();
 		JFrame frame = new JFrame("beeroverflow");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//ImageIcon imgThisImg = new ImageIcon("logo.jpg");
-		//JLabel pilt = new JLabel(imgThisImg);
-		//pilt.setIcon(imgThisImg);
-		JLabel textLabel = new JLabel("<html>Team name: Beeroverflow<br>"
-				+ "Team leader: Rene Juuse<br>"
-				+ "Team leader e-mail: rene.juuse@gmail.com<br>"
-				+ "Team members: Iris Merilo, Airis Kruus, Ann Lember<br>"
-				+ "Logo: <br>"
-				+ "<IMG SRC=http://www.manatee-lanes.com/wp-content/uploads/2012/04/iStock_000018060649XSmall.jpg>  </html>",SwingConstants.CENTER);
+		while (enuKeys.hasMoreElements()) {
+			String key = (String) enuKeys.nextElement();
+			String value = properties.getProperty(key);
+			textLabel.setText( textLabel.getText() +"<html>"+ key.replace('_', ' ') + ": " + value + " <br>"+ "<html>");
+		}	
+			
 		frame.getContentPane().add(textLabel, BorderLayout.PAGE_START);
-		//frame.getContentPane().add(pilt, BorderLayout.AFTER_LINE_ENDS);
-		textLabel.setPreferredSize(new Dimension(300, 500));
+		frame.getContentPane().add(pilt, BorderLayout.AFTER_LINE_ENDS);
+		textLabel.setPreferredSize(new Dimension(180, 150));
 		frame.pack();
-		frame.setVisible(true);		
-	}
-	
-}
+		frame.setVisible(true);	
