91eeffc66c69b05472766660dda530781345d1c1
airis94 <airis.kruus@gmail.com>
2014-10-14 19:37:51 +0300

    Update IntroUI.java

--- POS/src/ee/ut/math/tvt/beeroverflow/IntroUI.java
+++ POS/src/ee/ut/math/tvt/beeroverflow/IntroUI.java
@@ -1,7 +1,6 @@
 package ee.ut.math.tvt.beeroverflow;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,15 +12,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class IntroUI extends JPanel{
-	
-	public static void createWindow() throws IOException{
+public class IntroUI extends JPanel {
+	public static void createWindow() throws IOException {
 		JFrame frame = new JFrame("beeroverflow");
 		JPanel panel = new JPanel(new BorderLayout());
 		JLabel textLabel = new JLabel();
-		ImageIcon img = new ImageIcon("img//beer.jpg");
-		JLabel pilt = new JLabel(img);
-		pilt.setIcon(img);
 		File file = new File("application.properties");
 		FileInputStream fileInput = new FileInputStream(file);
 		Properties properties = new Properties();
@@ -32,12 +27,19 @@ public class IntroUI extends JPanel{
 		while (enuKeys.hasMoreElements()) {
 			String key = (String) enuKeys.nextElement();
 			String value = properties.getProperty(key);
-			textLabel.setText( textLabel.getText() +"<html>"+ key.replace('_', ' ') + ": " + value + " <br>"+ "<html>");
-		}	
-			
+			if (key.contains("logo")) {
+				ImageIcon img = new ImageIcon(value);
+				JLabel pilt = new JLabel(img);
+				pilt.setIcon(img);
+				panel.add(pilt, BorderLayout.SOUTH);
+				textLabel.setText(textLabel.getText() + "<html>" + key.replace('_', ' ') + ": " + " <br>" + "<html>");
+			} 
+			else {
+				textLabel.setText(textLabel.getText() + "<html>" + key.replace('_', ' ') + ": " + value + " <br>" + "<html>");
+			}
+		}
 		panel.add(textLabel, BorderLayout.NORTH);
-	    	panel.add(pilt, BorderLayout.SOUTH);
-	    	frame.add(panel, BorderLayout.NORTH);
+		frame.add(panel, BorderLayout.NORTH);
 		frame.pack();
 		frame.setVisible(true);
 	}
