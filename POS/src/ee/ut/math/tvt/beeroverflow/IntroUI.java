7294ad749ed335ee8d0b273d77e358f29ccc3006
Rene Juuse <rene.juuse@gmail.com>
2014-10-14 20:48:09 +0300

    Loeb sisse ka versiooni numbri. Logo asukoht loetakse nüüd app prop failist. Modifitseerisin teostust ning (loodetavasti) muutsin koodi paremini loetavaks.

--- POS/src/ee/ut/math/tvt/beeroverflow/IntroUI.java
+++ POS/src/ee/ut/math/tvt/beeroverflow/IntroUI.java
@@ -1,13 +1,13 @@
 package ee.ut.math.tvt.beeroverflow;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Properties;
 
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,31 +15,64 @@ import javax.swing.JPanel;
 
 public class IntroUI extends JPanel{
 	
-	public static void createWindow() throws IOException{
-		JFrame frame = new JFrame("beeroverflow");
-		JPanel panel = new JPanel(new BorderLayout());
-		JLabel textLabel = new JLabel();
-		ImageIcon img = new ImageIcon("img//beer.jpg");
+	public static void createWindow(){
+		JLabel textLabel;
+		Properties appProperties;
+		Properties verProperties;
+		JFrame frame = new JFrame("BeerOverflow");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		File appFile = new File("application.properties");
+		File verFile = new File("version.properties");
+		String teamKey = "Our_team_logo";
+		String verKey = "version.number";
+	    Box box = Box.createVerticalBox();
+
+		//Loading the files in
+		appProperties = makeProperties(appFile);
+		verProperties = makeProperties(verFile);
+
+	    // Deals with application.properties labels
+		Enumeration<Object> appKeys = appProperties.keys();
+		while (appKeys.hasMoreElements()) {
+			String key = (String) appKeys.nextElement();
+			String value = appProperties.getProperty(key);
+			if(!key.equals(teamKey)) {
+				System.out.println(value);
+				textLabel = new JLabel(key.replace('_', ' ') + ": " + value);
+				box.add(textLabel);
+			}
+		}
+		
+		// Insert the version number
+		String versionNum = verProperties.getProperty(verKey);
+		box.add(new JLabel("Version: " + versionNum));
+		
+		// Insert the logo
+		ImageIcon img = new ImageIcon(appProperties.getProperty(teamKey));
 		JLabel pilt = new JLabel(img);
 		pilt.setIcon(img);
-		File file = new File("application.properties");
-		FileInputStream fileInput = new FileInputStream(file);
-		Properties properties = new Properties();
-		properties.load(fileInput);
-		fileInput.close();
-		Enumeration<Object> enuKeys = properties.keys();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		while (enuKeys.hasMoreElements()) {
-			String key = (String) enuKeys.nextElement();
-			String value = properties.getProperty(key);
-			textLabel.setText( textLabel.getText() +"<html>"+ key.replace('_', ' ') + ": " + value + " <br>"+ "<html>");
-		}	
-			
-		panel.add(textLabel, BorderLayout.NORTH);
-	    	panel.add(pilt, BorderLayout.SOUTH);
-	    	frame.add(panel, BorderLayout.NORTH);
+		box.add(pilt);
+		
+		// Compose the frame
+		frame.add(box);
 		frame.pack();
 		frame.setVisible(true);
+	}
+	
+	private static Properties makeProperties(File f){
+		FileInputStream fis;
+		Properties prop = new Properties();
+		try {
+			fis = new FileInputStream(f);
+			prop = new Properties();
+			prop.load(fis);
+			fis.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException ioe){
+			ioe.printStackTrace();
+		}
+		return prop;
 	}
 
 }
