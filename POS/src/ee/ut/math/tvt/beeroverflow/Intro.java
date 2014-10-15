01e76f330a29ce0693be3f6e3383bacda27abb99
Rene Juuse <rene.juuse@gmail.com>
2014-10-15 14:53:56 +0300

    Revert "Branch change"
    This reverts commit 37cbf633e3ecb5e16d21bd3b44aa94af8f913d09.

--- POS/src/ee/ut/math/tvt/beeroverflow/Intro.java
+++ POS/src/ee/ut/math/tvt/beeroverflow/Intro.java
@@ -1,46 +1,12 @@
 package ee.ut.math.tvt.beeroverflow;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
-import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
-import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
-import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
-
-
 
 public class Intro {
 
-private static final Logger log = LogManager.getLogger(Intro.class.getName());
-private static final String MODE = "console";
-
 	public static void main(String[] args) {
-	
-		final SalesDomainController domainController = new SalesDomainControllerImpl();
-	
-		if (args.length == 1 && args[0].equals(MODE)) {
-			log.debug("Mode: " + MODE);
-	
-			ConsoleUI cui = new ConsoleUI(domainController);
-			cui.run();
-		} else {
-	
-			IntroUI introUI = new IntroUI();
-			introUI.setVisible(true);
-			//introUI.setAlwaysOnTop(true);
-	
-			final SalesSystemUI ui = new SalesSystemUI(domainController);
-			ui.setVisible(true);
-	
-			//introUI.setAlwaysOnTop(false);
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			//introUI.setVisible(false);
-		}
-	}
-}
 
+		IntroUI.createWindow();
+		
+	}
+	 
+}
